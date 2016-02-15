85ba9a3cb96b33986e45b7a3e3a9b9e23698e40a
Rasmus Lager <rasmus.lager@chas.se>
2016-02-15 12:53:52 +0100

    setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ganapi',
+      packages = ['ganapi'],
+      version='0.1',
+      description='Get a Newsletter REST API wrapper',
+      license='MIT',
+      author='Get a Newsletter',
+      author_email='tech@getanewsletter.com',
+      url='https://github.com/getanewsletter/api-python',
+      keywords= ['Get a newsletter', 'ganapi'],
+      download_url='https://github.com/getanewsletter/api-python/tarball/0.1',
+      classifiers=[])
